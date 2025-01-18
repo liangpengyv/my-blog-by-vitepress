8f507c1efb49a190de549980d3a9c4673659c26f
liangpengyv <me@laoliang.ink>
2025-01-18 18:14:07 +0800

    feat: 添加文章搜索功能

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -24,6 +24,28 @@ export default defineConfig({
       { text: '关于', link: '/about/index', activeMatch: '/about/' },
     ],
 
+    search: {
+      provider: 'local',
+      options: {
+        translations: {
+          button: {
+            buttonText: '搜索文章',
+            buttonAriaLabel: '搜索文章',
+          },
+          modal: {
+            noResultsText: '无法找到相关结果',
+            resetButtonTitle: '清除查询条件',
+            displayDetails: '显示详细列表',
+            footer: {
+              selectText: '选择',
+              navigateText: '切换',
+              closeText: '关闭',
+            },
+          },
+        },
+      },
+    },
+
     logo: '/avatar.png',
     socialLinks: [
       // {
