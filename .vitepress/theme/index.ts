79adee23a0908f82f0568d35fbc1a05fccddd309
liangpengyv <me@laoliang.ink>
2025-01-19 21:18:23 +0800

    fix: 取消微信内置浏览器检测提醒

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -12,11 +12,6 @@ export default {
     })
   },
   enhanceApp({ app, router, siteData }) {
-    if (typeof window !== 'undefined') {
-      const isWechat = /micromessenger/i.test(navigator.userAgent)
-      if (isWechat) {
-        alert('可能无法加载某些外部资源\n\n建议使用外部浏览器打开\n\n以获得最佳浏览体验')
-      }
-    }
+    // ...
   },
 } satisfies Theme
